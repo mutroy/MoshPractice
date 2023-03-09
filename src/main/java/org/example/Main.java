babf22a696fa4a198e951887db4a015c8baa2e57
Mohan <mericajam@gmail.com>
2023-03-09 16:26:49 -0500

    Created develop branch and modified the file

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,8 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
+
+        for(int i = 0;i<109;i++)
         System.out.println("Varahi Amma Namo Namah");
     }
 }
